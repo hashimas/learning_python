937a61b6e96327642b1061b48557fa58c8d90639
Hashim Ismail <hashim.ismaila@stetis.com>
2018-09-19 09:06:01 +0100

    Data Structure and Module lessons

---
+++ module_test.py
@@ -0,0 +1,5 @@
+import module
+module.fibonacci(2000)
+print(module.fibonacci2(30000))
+fib = module.fibonacci
+fib(200)
---
+++ module_test2.py
@@ -0,0 +1,3 @@
+from module import fibonacci,fibonacci2
+fibonacci(200)
+print(fibonacci2(200))
