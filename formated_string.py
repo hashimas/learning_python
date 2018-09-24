07a7636ffaad5081d1de29353de2d3820a3fc294
Hashim Ismail <hashim.ismaila@stetis.com>
2018-09-24 09:44:08 +0100

    Input and output

---
+++ formated_string.py
@@ -0,0 +1,2 @@
+print('{} is  {} years old'.format('Hashim',repr(26)))
+print('{1} is {0} years old'.format('Hashim',repr(26)))
---
+++ input_output.py
@@ -0,0 +1,10 @@
+message = 'Hello World!'
+message2 = 'Hello World! \n'
+number = 32/35
+display = message + repr(number)
+display2 = message + str(number)
+display3 = message2 + repr(number)
+print(display)
+print(display2)
+print(display3)
+print(repr(display3))
